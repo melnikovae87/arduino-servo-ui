e7beb71117c24235d8d54ef83cd4e5510cfa1369
Artem Melnikov <melnikovae87@gmail.com>
2024-05-29 15:33:49 +0300

    Update layout

---
+++ README.md
@@ -0,0 +1,14 @@
+# UI Arduino Servo Control
+
+### Specification
+```text
+MODEL = Arduino UNO/NANO
+DIGITAL_PIN = 9
+TYPE = PWM
+SERIAL_BAUD_RATE = 9600
+SERIAL_PORT = by default detect Arduino device
+
+Communication keys:
+PING - check RX/TX communication
+ANGL<90> - set servo angle for connected device
+```
--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from serial.serialutil import SerialBase
 class App(ctk.CTk):
     ctk.set_appearance_mode("dark")
     ctk.set_default_color_theme("green")
+    ctk.deactivate_automatic_dpi_awareness()
 
     def __init__(self):
         super().__init__()
@@ -18,7 +19,12 @@ class App(ctk.CTk):
         self.max_angle: int = 180
         self.current_angle: int = 0
 
+        self._label_font_size = 16
+        self._main_font_size = 14
+        self._small_font_size = 14
+
         self._update_job_id = None
+
         self.serial_port: SerialBase | None = None
         self.port_list = serial.tools.list_ports.comports()
         self.baudrate_list = ["75", "110", "300", "1200", "2400", "4800", "9600", "19200", "38400", "57600", "115200"]
@@ -34,7 +40,8 @@ class App(ctk.CTk):
         self.message_box: ctk.CTkTextbox | None = None
 
         self.title("UI Arduino Servo Control")
-        self.geometry("650x650")
+        self.geometry("500x520")
+        self.resizable(False, False)
 
         self.create_widgets()
 
@@ -46,7 +53,7 @@ class App(ctk.CTk):
 
     def sliding_callback(self, value):
         angle = int(value)
-        self.angle_label.configure(text=f"Servo Angle: {angle}")
+        self.angle_label.configure(text=f"Servo Angle: {angle} °")
 
         if self._update_job_id is not None:
             self.after_cancel(self._update_job_id)
@@ -124,94 +131,120 @@ class App(ctk.CTk):
         self.print_message(f"Port closed.")
 
     def create_widgets(self):
+
+        ctk.CTkLabel(
+            master=self,
+            height=30,
+            text=f"Range of servo data [{self.min_angle} - {self.max_angle}] degree",
+            font=("Helvetica", self._main_font_size, "normal")
+        ).pack(pady=(10, 0), fill="x")
+
         # Angle label
         self.angle_label = ctk.CTkLabel(
             master=self,
-            text="Servo Angle: 90",
-            font=("Helvetica", 20)
+            height=30,
+            text="Servo Angle: 90 °",
+            font=("Helvetica", self._label_font_size, "bold")
         )
-        self.angle_label.pack(pady=(50, 10))
+        self.angle_label.pack(pady=0, fill="x")
 
         # Angle Slider
         self.angle_slider = ctk.CTkSlider(
             master=self,
             from_=self.min_angle,
             to=self.max_angle,
-            width=550,
-            height=30,
+            height=20,
             progress_color="transparent",
             command=self.sliding_callback,
             orientation="horizontal")
-        self.angle_slider.pack(pady=20)
+        self.angle_slider.pack(pady=10, padx=20, fill="x")
         self.angle_slider.set(90)
 
         btn_frame = ctk.CTkFrame(self)
-        btn_frame.pack(pady=20)
+        btn_frame.pack(pady=(10, 0), padx=20)
 
         # Select Communication Port
         self.select_port_btn = ctk.CTkComboBox(
             master=btn_frame,
-            width=400,
+            height=30,
+            width=440,
+            justify="center",
             values=self.available_ports_callback(),
             font=("Helvetica", 16),
-            dropdown_font=("Helvetica", 16),
-            justify="center",
+            dropdown_font=("Helvetica", self._main_font_size),
             command=self.port_callback)
-        self.select_port_btn.grid(row=0, column=0, columnspan=2, padx=20, pady=(20, 10), sticky="ew")
+        self.select_port_btn.grid(row=0, column=0, columnspan=2, pady=(10, 0), padx=10, sticky="wens")
         self.select_port_btn.set(self.set_default_arduino_port())
 
-        # Select Serial Speed
+        # Select Serial Speed btn
         self.select_baudrate_btn = ctk.CTkComboBox(
             master=btn_frame,
+            height=30,
             values=self.baudrate_list,
-            width=400,
-            font=("Helvetica", 16),
+            font=("Helvetica", self._main_font_size),
             justify="center",
-            dropdown_font=("Helvetica", 16),
+            dropdown_font=("Helvetica", self._main_font_size),
             command=self.baudrate_callback)
-        self.select_baudrate_btn.grid(row=1, column=0, columnspan=2, padx=20, pady=(10, 10), sticky="ew")
+        self.select_baudrate_btn.grid(row=1, column=0, columnspan=2, pady=(5, 0), padx=10, sticky="wens")
         self.select_baudrate_btn.set("9600")
 
-        # Open Connection
+        # Open Connection btn
         self.start_btn = ctk.CTkButton(
             master=btn_frame,
-            width=200,
+            height=30,
             text="Start",
-            font=("Helvetica", 16),
+            font=("Helvetica", self._main_font_size),
             command=self.open_port)
-        self.start_btn.grid(row=2, column=0, padx=20, pady=10, sticky="ew")
+        self.start_btn.grid(row=2, column=0, pady=(5, 0), padx=(10, 5), sticky="wens")
 
-        # Close Connection
+        # Close Connection btn
         self.stop_btn = ctk.CTkButton(
             master=btn_frame,
-            width=200,
+            height=30,
             text="Stop",
-            font=("Helvetica", 16),
+            font=("Helvetica", self._main_font_size),
             command=self.close_port)
-        self.stop_btn.grid(row=2, column=1, padx=20, pady=10, sticky="ew")
+        self.stop_btn.grid(row=2, column=1, pady=(5, 0), padx=(5, 10), sticky="wens")
 
+        ctk.CTkLabel(
+            master=btn_frame,
+            height=30,
+            anchor="w",
+            text="Communication Terminal",
+            font=("Helvetica", self._main_font_size)
+        ).grid(row=3, column=0, columnspan=2, pady=(20, 0), padx=(20, 0), sticky="wens")
+
+        # Terminal message area
         self.message_box = ctk.CTkTextbox(
             master=btn_frame,
-            width=400,
-            height=200,
-            font=("Helvetica", 13))
-        self.message_box.grid(row=3, column=0, columnspan=2, padx=20, pady=10, sticky="ew")
+            height=150,
+            font=("Helvetica", self._small_font_size))
+        self.message_box.grid(row=4, column=0, columnspan=2, pady=0, padx=10, sticky="wens")
 
+        # Ping Serial btn
         self.ping_btn = ctk.CTkButton(
             master=btn_frame,
-            width=200,
+            height=30,
             text="Ping",
-            font=("Helvetica", 16),
+            font=("Helvetica", self._main_font_size),
             command=self.ping_serial)
-        self.ping_btn.grid(row=4, column=0, padx=20, pady=(10, 20), sticky="ew")
+        self.ping_btn.grid(row=5, column=0, pady=(5, 10), padx=(10, 5), sticky="wens")
 
+        # Clear terminal messages btn
         self.clear_message_btn = ctk.CTkButton(
             master=btn_frame,
-            width=200,
+            height=30,
             text="Clear",
-            font=("Helvetica", 16),
+            font=("Helvetica", self._main_font_size),
             command=self.clear_message)
-        self.clear_message_btn.grid(row=4, column=1, padx=20, pady=(10, 20), sticky="ew")
+        self.clear_message_btn.grid(row=5, column=1, pady=(5, 10), padx=(5, 10), sticky="wens")
+
+        ctk.CTkLabel(
+            master=self,
+            height=30,
+            text="Copyright © 2024 Designed by Artem Melnikov",
+            font=("Helvetica", 12, "normal")
+        ).pack(fill="x")
 
 
 if __name__ == "__main__":
